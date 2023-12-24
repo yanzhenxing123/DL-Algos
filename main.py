3120457984e25bc5f8c39e664eb6bca17bd00c56
yanzhenxing123 <209243446@qq.com>
2023-12-24 18:28:33 +0800

    feat: all the models

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+"""
+@Time: 2023/12/24 17:56
+@Author: yanzx
+@Description: 
+"""
 import torch
 from torch import nn
 from torch.utils.data import (
@@ -13,6 +18,8 @@ num_samples = 100
 num_features = 3
 batch_size = 4
 lr = 0.01
+
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 class MyDataSet(Dataset):
@@ -61,19 +68,57 @@ def train(model, dataloader, epochs):
         logger.info(f"epoch {epoch} loss: {avg_loss:.4f}")
 
 
-def main():
-    # 1. 生成数据
+def test(model, testloader):
+    """Validate the network on the entire test set."""
+    criterion = nn.MSELoss()
+    losses = []
+    with torch.no_grad():
+        for data in testloader:
+            X_test, y_test = data['data'], data['label']
+            X_test, y_test = X_test.to(DEVICE), y_test.to(DEVICE)
+            outputs = model(X_test)
+            loss = criterion(outputs, y_test.view(-1, 1))
+            losses.append(loss)
+    logger.info(f"Total avg loss:{sum(losses) / len(losses):.4f}")
+    return loss
+
+
+def get_data():
+    """
+    模拟生成数据
+    :return:
+    """
     data = torch.randn(num_samples, num_features)
     labels = []
     for data_e in data:
         y_e = data_e[0] + 2 * data_e[1] + 3 * data_e[2]
         labels.append(y_e.item())
     labels = torch.tensor(labels)
+    return data, labels
+
+
+def main():
+    # 1. 生成数据
+    data, labels = get_data()
     my_dataset = MyDataSet(data, labels)
-    my_dataloader = DataLoader(my_dataset, batch_size=batch_size, shuffle=True)
     # 2. 训练
+    train_size = int(0.8 * len(my_dataset))
+    test_size = len(my_dataset) - train_size
+    train_dataset, test_dataset = random_split(my_dataset, [train_size, test_size])
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
     model = Net(input_size=num_features, output_size=1)
-    train(model, my_dataloader, epochs)
+    train(model, train_dataloader, epochs)
+    # 3. 保存模型
+    torch.save(model.state_dict(), 'data/model/demo_model.pth')
+    # 4. 测试
+    model = Net(input_size=num_features, output_size=1)
+    model.load_state_dict(torch.load("data/model/demo_model.pth"))
+    test(model, test_dataloader)
+
+    # 5. 查看参数信息
+    for key, value in model.state_dict().items():
+        print(key, value)
 
 
 if __name__ == '__main__':
